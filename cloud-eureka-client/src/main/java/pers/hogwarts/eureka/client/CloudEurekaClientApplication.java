21d5838a2f584a74964047a6208e96af17a38c08
hogwarts <linxiuke@outlook.com>
2019-06-06 17:30:06 +0800

    eureka

---
+++ cloud-eureka-client/src/main/java/pers/hogwarts/eureka/client/CloudEurekaClientApplication.java
@@ -0,0 +1,16 @@
+package pers.hogwarts.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CloudEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaClientApplication.class, args);
+    }
+
+}
