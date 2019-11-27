4a203c7a1606bde5d30bd0c45ba759f8e5a6ca05
Hogwarts <linxiuke@outlook.com>
2019-11-27 11:33:52 +0800

    spring cloud config  注册到Eureka

--- cloud-config-server/src/main/java/pers/hogwarts/cloudconfigserver/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/pers/hogwarts/cloudconfigserver/CloudConfigServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient    没有注册启动后会报错但是不影响
+@EnableEurekaClient    //没有注册启动后会报错但是不影响
 @EnableConfigServer
 @SpringBootApplication
 public class CloudConfigServerApplication {
