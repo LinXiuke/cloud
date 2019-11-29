2fb392017f349daabdd2966b29fffd041ed49994
Hogwarts <linxiuke@outlook.com>
2019-11-29 11:07:30 +0800

    Hystrix Dashboard

--- cloud-eureka-client/src/main/java/pers/hogwarts/eureka/client/CloudEurekaClientApplication.java
+++ cloud-eureka-client/src/main/java/pers/hogwarts/eureka/client/CloudEurekaClientApplication.java
@@ -1,17 +1,24 @@
 package pers.hogwarts.eureka.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
+//  HystrixDashboard
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class CloudEurekaClientApplication {
 
     public static void main(String[] args) {
@@ -22,7 +29,12 @@ public class CloudEurekaClientApplication {
     String port;
 
     @RequestMapping("/hello")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam String name) {
         return "hello "+name+",i am from port:" +port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
--- cloud-server-feign/src/main/java/pers/hogwarts/server/feign/ServerFeignApplication.java
+++ cloud-server-feign/src/main/java/pers/hogwarts/server/feign/ServerFeignApplication.java
@@ -2,13 +2,20 @@ package pers.hogwarts.server.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServerFeignApplication {
 
     public static void main(String[] args) {
