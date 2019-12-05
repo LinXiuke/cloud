8b01d8875c9641b2b3e8e0bae03f1a218c334bf8
Hogwarts <linxiuke@outlook.com>
2019-12-05 16:42:46 +0800

    gateway 路由

--- cloud-gateway/src/main/java/pers/hogwarts/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/pers/hogwarts/cloud/gateway/CloudGatewayApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class CloudGatewayApplication {
 
@@ -18,7 +20,7 @@ public class CloudGatewayApplication {
     }
 
 
-    @Bean
+//    @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 
         String httpUri = "http://httpbin.org:80";
